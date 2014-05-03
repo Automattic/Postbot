aaa425fba31089685ccdbdb1abdb8818cd87d1ff
Joen Asmussen <joen@automattic.com>
2014-05-03 19:58:04 +0200

    Postbot: Add webm video format and mimetype to .htaccess for Firefox compatibility.

--- public_html/wpcc.php
+++ public_html/wpcc.php
@@ -75,7 +75,10 @@ if ( isset( $_GET['msg'] ) ) {
 			</p>
 
 			<div class="postbot-anim">
-				<video width="752" height="376" autoplay="autoplay" src="//postbot.co/postbot.mp4" type="video/mp4" loop="" id="video">
+				<video width="752" height="376" autoplay="autoplay" loop="" id="video">
+					<source src="//postbot.co/postbot.mp4" type="video/mp4">
+					<source src="http://moc.co/sandbox/test/postbot.webm" type="video/webm">
+
 					<object data="//postbot.co/postbot.mp4" width="752" height="376">
 						<embed src="//postbot.co/postbot.mp4" width="752" height="376"/>
 						Your browser does not support video
